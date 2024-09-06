564f6cdf5177ff95732d8bc865aea6275eb70275
thePalma <dipalma.antonio98@gmail.com>
2024-09-06 16:16:03 +0200

    Initial commit

---
+++ src/main/java/com/github/thepalma/spend_sage/SpendSageApplication.java
@@ -0,0 +1,13 @@
+package com.github.thepalma.spend_sage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpendSageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpendSageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/thepalma/spend_sage/SpendSageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.thepalma.spend_sage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpendSageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
